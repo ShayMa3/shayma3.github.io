988879c1c2a509e1e2314963862a92345989ddec
Shay Ma <s.ma5102@gmail.com>
2020-05-01 16:35:51 -0700

    my website
    wdd

---
+++ personal website/assets/js/script.js
@@ -0,0 +1,44 @@
+// import jump from 'jump.js'
+// type = "module";
+// import jump from './node_modules/jump.js/dist/jump.module.js';
+
+// var about_button = document.getElementById("aboutbtn");
+// about_button.addEventListener('click', () =>{
+//   jump('.target2')
+// })
+
+new TypeIt("#simpleUsage", {
+    // strings: "a personal webpage ",
+    speed: 50,
+    waitUntilVisible: true
+
+  })
+
+  .exec(async () => {
+    //-- Return a promise that resolves after something happens.
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        return resolve();
+      }, 1400);
+    });
+  })
+  .type("a personal webpage ")
+  .go();
+
+let about_button = document.getElementById("aboutbtn");
+about_button.onclick = function(){
+  Jump(900);
+};
+
+let design_button = document.getElementById("designbtn");
+design_button.onclick = function(){
+  Jump(1340);
+}
+
+let contact_button = document.getElementById("contactbtn");
+contact_button.onclick = function(){
+  Jump(5000);
+}
+  
+  
+
